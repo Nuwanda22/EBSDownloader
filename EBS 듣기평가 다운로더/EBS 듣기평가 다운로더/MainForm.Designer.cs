b4a3ede35e9a94c059bf98184163f7627722045b
Nuwanda22 <njk230@gmail.com>
2016-03-30 22:00:17 +0900

    Add Download Progressbar
    ;)

--- EBS 듣기평가 다운로더/EBS 듣기평가 다운로더/MainForm.Designer.cs
+++ EBS 듣기평가 다운로더/EBS 듣기평가 다운로더/MainForm.Designer.cs
@@ -28,21 +28,22 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.downloadUrlTextBox = new System.Windows.Forms.TextBox();
 			this.button1 = new System.Windows.Forms.Button();
 			this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+			this.downloadProgressBar = new System.Windows.Forms.ProgressBar();
 			this.SuspendLayout();
 			// 
-			// textBox1
+			// downloadUrlTextBox
 			// 
-			this.textBox1.Location = new System.Drawing.Point(12, 12);
-			this.textBox1.Name = "textBox1";
-			this.textBox1.Size = new System.Drawing.Size(260, 21);
-			this.textBox1.TabIndex = 0;
+			this.downloadUrlTextBox.Location = new System.Drawing.Point(12, 12);
+			this.downloadUrlTextBox.Name = "downloadUrlTextBox";
+			this.downloadUrlTextBox.Size = new System.Drawing.Size(260, 21);
+			this.downloadUrlTextBox.TabIndex = 0;
 			// 
 			// button1
 			// 
-			this.button1.Location = new System.Drawing.Point(197, 54);
+			this.button1.Location = new System.Drawing.Point(197, 75);
 			this.button1.Name = "button1";
 			this.button1.Size = new System.Drawing.Size(75, 23);
 			this.button1.TabIndex = 1;
@@ -54,15 +55,24 @@
 			// 
 			this.saveFileDialog.Filter = "mp3 File|*.mp3";
 			// 
+			// downloadProgressBar
+			// 
+			this.downloadProgressBar.Location = new System.Drawing.Point(12, 39);
+			this.downloadProgressBar.Name = "downloadProgressBar";
+			this.downloadProgressBar.Size = new System.Drawing.Size(260, 23);
+			this.downloadProgressBar.TabIndex = 2;
+			this.downloadProgressBar.Visible = false;
+			// 
 			// MainForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(284, 87);
+			this.ClientSize = new System.Drawing.Size(284, 110);
+			this.Controls.Add(this.downloadProgressBar);
 			this.Controls.Add(this.button1);
-			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.downloadUrlTextBox);
 			this.Name = "MainForm";
-			this.Text = "Form1";
+			this.Text = "EBS 다운로더";
 			this.ResumeLayout(false);
 			this.PerformLayout();
 
@@ -70,9 +80,10 @@
 
 		#endregion
 
-		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.TextBox downloadUrlTextBox;
 		private System.Windows.Forms.Button button1;
 		private System.Windows.Forms.SaveFileDialog saveFileDialog;
+		private System.Windows.Forms.ProgressBar downloadProgressBar;
 	}
 }
 
--- EBS 듣기평가 다운로더/EBS 듣기평가 다운로더/MainForm.cs
+++ EBS 듣기평가 다운로더/EBS 듣기평가 다운로더/MainForm.cs
@@ -13,24 +13,35 @@ namespace EBS_듣기평가_다운로더
 {
 	public partial class MainForm : Form
 	{
-		string url = "http://wstrotu.ebs.co.kr/ebsvod/mid/2015/10014892/mp3/20150917_110000_mp3.mp3?key=eac13bff8e48aca43e0b11f82b94a39793dcb904";
+		string url = "http://wstrotu.ebs.co.kr/ebsvod/mid/2015/10014892/mp3/20150917_110000_mp3.mp3?key=6733f06e7e8cf25f5ab08a3ddb47f6edb2ea6a15";
 		string fileName = "20150917_110000_mp3.mp3";
+		WebClient webClient;
 
 		public MainForm()
 		{
 			InitializeComponent();
+
+			webClient = new WebClient();
+			webClient.DownloadProgressChanged += (sender, e) =>
+			{
+				downloadProgressBar.Value = e.ProgressPercentage;
+			};
+			webClient.DownloadFileCompleted += (sender, e) =>
+			{
+				MessageBox.Show("다운로드가 완료되었습니다!");
+				downloadProgressBar.Visible = false;
+			};
 		}
 
 		private void button1_Click(object sender, EventArgs e)
 		{
-			using (WebClient webClient = new WebClient())
+			saveFileDialog.FileName = fileName;
+			if (saveFileDialog.ShowDialog() == DialogResult.OK)
 			{
-				saveFileDialog.FileName = fileName;
-				if (saveFileDialog.ShowDialog() == DialogResult.OK)
-				{
-					webClient.DownloadFile(url, saveFileDialog.FileName);
-				}
+				downloadProgressBar.Visible = true;
+				webClient.DownloadFileAsync(new Uri(url), saveFileDialog.FileName);
 			}
+			webClient.Dispose();
 		}
 	}
 }
